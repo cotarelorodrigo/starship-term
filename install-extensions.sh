789e11faaeced6e50fb47f4a36bdb52b27f13038
cotarelorodrigo <cotarelorodrigo@gmail.com>
2022-03-15 16:51:36 -0300

    Create install-extensions.sh

---
+++ install-extensions.sh
@@ -0,0 +1,6 @@
+code --install-extension dbaeumer.vscode-eslint --force &
+code --install-extension dsznajder.es7-react-js-snippets --force &
+code --install-extension JuanBlanco.solidity --force &
+code --install-extension ms-azuretools.vscode-docker --force &
+code --install-extension ms-vscode-remote.remote-containers --force &
+code --install-extension rangav.vscode-thunder-client --force
